e9d3a23e4ac0f8d06aba018fef9bfb2d9c1b7d78
unknown <akhileshcowlessur54@gmail.com>
2026-07-24 14:20:45 +0400

    🔧 Fix Vercel build: prioritize cloned Flutter in PATH and fix flag syntax

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Install Flutter
 git clone https://github.com/flutter/flutter.git -b stable
-export PATH="$PATH:`pwd`/flutter/bin"
+export PATH="`pwd`/flutter/bin:$PATH"
+
+# Debug version
+flutter --version
 
 # Get dependencies
 flutter pub get
 
-# Build the web app with HTML renderer for best compatibility
-flutter build web --release --web-renderer html
+# Build the web app with HTML renderer
+flutter build web --release --web-renderer=html
